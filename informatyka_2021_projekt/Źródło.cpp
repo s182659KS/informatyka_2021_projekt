88e7020b075bad46838c4dbce54f498ee36ec7f0
s182659KS <93378192+s182659KS@users.noreply.github.com>
2021-11-28 20:29:35 +0100

    Add files via upload
    Dodanie ograniczenia dla ruchu postaci w oknie i generowanie otoczenia

--- informatyka_2021_projekt/Źródło.cpp
+++ informatyka_2021_projekt/Źródło.cpp
@@ -11,7 +11,8 @@ za ka¿dym trafieniem. Punkty zdobywamy poprzez liczbe zniszczonych przeciwników 
 #include <iostream>
 #include <random>
 #include <math.h>
-
+#pragma warning(disable:4996)
+//GRACZ
 class Player {//klasa dla gracza
 private:
 	sf::Texture plrtxt;//tekstura dla gracza
@@ -38,10 +39,9 @@ public:
 	void draw(sf::RenderWindow& window) {
 			window.draw(plrtank);
 	}
-	float ruch() {//sterowanie pojazdem gracza
+	float ruch(sf::RenderWindow& window) {//sterowanie pojazdem gracza
 		float rotacja;
 		float pi = 3.14159;
-		int mark = zderzenie();
 		rotacja = plrtank.getRotation()-90;
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
 			plrtank.move(cos(rotacja*pi/180), sin(rotacja * pi / 180));
@@ -51,15 +51,23 @@ public:
 			plrtank.rotate(-1);
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
 			plrtank.rotate(1);
-		std::cout << "Rotacja:" << rotacja << std::endl;
+		if(plrtank.getPosition().x <= 0)
+			plrtank.move(1, 0);
+		if (plrtank.getPosition().x >= window.getSize().x)
+			plrtank.move(-1, 0);
+		if (plrtank.getPosition().y <= 0)
+			plrtank.move(0, 1);
+		if (plrtank.getPosition().y >= window.getSize().y)
+			plrtank.move(0, -1);
+		//std::cout << "Rotacja:" << rotacja << std::endl;
 		
 		return rotacja;
 	}
 	int zderzenie() {//domyœlnie bêdzie pobiera³ rozmaiar okna
-		int marker;
-		return marker;
+
 	}
 };
+//PRZECIWNICY
 class Enemy {//klasa dla botów -  przeciwników
 private:
 	sf::Texture enmtxt;//tekstura
@@ -67,8 +75,66 @@ private:
 	int N;//liczba obiektow graf
 	std::random_device rd;//randomizacja po³o¿enia 
 };
-class ObjSrd {//elementy otoczenia
 
+//OTOCZENIE
+class ObjSrd {//elementy otoczenia
+	sf::Sprite* wall;
+	sf::Texture walltex;
+	FILE* fp;
+	int mapa[50][2];//tablica przechowuj¹ca pozycje
+	sf::Vector2f pozycja;
+	std::random_device rd;
+	
+
+public:
+	int n = 50;
+	ObjSrd(int N) {
+		walltex.loadFromFile("cegla.png");
+		wall = new sf::Sprite[N];
+		fp = fopen("mapa1.txt", "rb");
+		for (int i = 0; i < N; i++) {
+			fread(&mapa[i][1], sizeof(mapa), 1, fp);
+			pozycja.x = mapa[i][1];
+			fread(&mapa[i][2], sizeof(mapa), 1, fp);
+			pozycja.y = mapa[i][2];
+			std::cout << mapa[i][1] << " " << mapa[i][2] << std::endl;
+			wall[i].setTexture(walltex);//nalozenie tekstury
+			wall[i].setPosition(sf::Vector2f(pozycja.x, pozycja.y));//ustawienie pozycji poczatkowej
+		}
+		std::fclose(fp);
+	}
+		
+
+	void generacja(int poziom) {//generatior rozmieszczenia elementów
+		int const N = 50;//tymczasowo, domyœlnie dynamiczna alokacja pam.
+		if (poziom == 1)
+			int const N = 50;
+		else if(poziom == 2)
+			int const N = 75;
+		else if (poziom == 3)
+			int const N = 100;
+		std::mt19937 gen(rd());
+		std::uniform_int_distribution<> distX(1, 27);
+		std::uniform_int_distribution<> distY(1, 20);
+		int tabB[N][2];
+		fp = fopen("mapa1.txt", "w+b");
+		for (int k = 0; k <= N; k++) {
+			int j = distX(gen);
+			int i = distY(gen);
+			tabB[k][1] = (j * 30)-15;
+			tabB[k][2] = (i * 30)-15;
+			std::cout << tabB[k][1] << "," << tabB[k][2] << std::endl;
+			fwrite(&tabB[k][1], sizeof(tabB[0][0]), 1, fp);
+			fwrite(&tabB[k][2], sizeof(tabB[0][0]), 1, fp);
+		}
+	}
+	void draw(sf::RenderWindow& window) {
+		for (int i = 0; i < n; i++)
+		{
+			//std::cout << "dzia³a";
+			window.draw(wall[i]);
+		}
+	}
 };
 
 int main()
@@ -78,6 +144,7 @@ int main()
 	unsigned int width = size.x;
 	unsigned int hight = size.y;
 	Player p1;
+	ObjSrd s1(50);
 	while (window.isOpen())
 	{
 	
@@ -89,7 +156,8 @@ int main()
 			
 		}
 		window.clear();
-		p1.ruch(); 
+		p1.ruch(window); 
+		s1.draw(window);
 		p1.draw(window);
 		window.display();
 	}
